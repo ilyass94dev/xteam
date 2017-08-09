9e6c49a0222d98cf453a3dfa1895d78e76afb51b
ilyass94dev <ilyassbensassi@gmail.com>
2017-08-09 18:55:03 +0000

    Message

---
+++ funcs.js
@@ -0,0 +1,96 @@
+var fs = require('fs')
+var path = require('path')
+var Promise = require('promise');
+/**
+ * General purpose data encoding
+ *
+ * (string): string
+ */
+function encode (data) {
+  return (new Buffer(data)).toString('base64')
+}
+
+/**
+ * Inverse of `encode`
+ *
+ * (string): string
+ */
+function decode (data) {
+  return (new Buffer('' + data, 'base64')).toString()
+}
+
+/**
+ * Encode a superhero name
+ *
+ * (string): string
+*/
+module.exports.encodeName = function (name) {
+  return encode('@' + name)
+}
+
+/**
+ * Load the database
+ *
+ * (string, (?Error, ?Object))
+ */
+module.exports.loadDb = function (dbFile, cb) {
+  fs.readFile(dbFile, function (err, res) {
+    if (err) { return cb(err) }
+
+    var messages
+    try {
+      messages = JSON.parse(res)
+    } catch (e) {
+      return cb(err)
+    }
+
+    return cb(null, { file: dbFile, messages: messages })
+  })
+}
+
+/**
+ * Find the user's inbox, given their encoded username
+ *
+ * (Object, string): Object
+ */
+module.exports.findInbox = function (db, encodedName) {
+  var messages = db.messages
+  return {
+    dir: path.dirname(db.file),
+    messages: Object.keys(messages).reduce(function (acc, key) {
+      if (messages[key].to === encodedName ) {
+        return acc.concat({
+          hash: key,
+          lastHash: messages[key].last,
+          from: messages[key].from
+        })
+      } else { return acc }
+    }, [])
+  }
+}
+
+/**
+ * Find the next message, given the hash of the previous message
+ *
+ * ({ messages: Array<Object> }, string): string
+ */
+ var message
+module.exports.findNextMessage = function (inbox, lastHash) {
+  // find the message which comes after lastHash
+  var found
+  for (var i = 0; i < inbox.messages.length; i++) {
+    if (inbox.messages[i].lastHash === lastHash) {
+      found = i
+      break
+    }
+  }
+
+fs.readFile(path.join(inbox.dir,inbox.messages[found].lastHash), 'utf8',function (err,data,next)
+ { if(err)
+   {
+     return err
+   }
+    message='from: ' + decode(inbox.messages[found].from) + '\n---\n' +  decode(data);
+    console.log(message)
+ })
+}
